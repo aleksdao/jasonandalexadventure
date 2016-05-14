964cc69342f3a27abc38f17a1ed1dc27ebd1c65f
Jiacheng Yang <jy1964@nyu.edu>
2016-05-14 02:01:31 -0400

    create user and activity models

---
+++ server/db/models/user.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+	firstName: {
+		type: String,
+		required: true
+	},
+	lastName: {
+		type: String,
+		required: true
+	},
+	age: {
+		type: Number,
+		required: true
+	},
+	//how to create FB profile?
+	matches: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User'
+	},
+	blocked: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User'
+	}
+});
+
+module.exports = mongoose.model('User', schema);
